9a6171509035bb126fb4354e8605731bac376057
Dreista <Dreista@users.noreply.github.com>
2025-03-31 22:35:23 +0800

    Fix Maven packaging

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,6 @@
-module com.example.kmcg {
+module com.apply.kmcg {
     requires javafx.controls;
     requires javafx.fxml;
-
-
     opens com.apply.kmcg to javafx.fxml;
     exports com.apply.kmcg;
 }
