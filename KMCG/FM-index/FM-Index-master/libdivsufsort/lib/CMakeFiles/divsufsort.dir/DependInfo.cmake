8308fc24a87386028e6422fe66306a5aa028ec90
周典 <144455527+WillChou1@users.noreply.github.com>
2025-04-03 18:44:16 +0800

    Add files via upload

---
+++ KMCG/FM-index/FM-Index-master/libdivsufsort/lib/CMakeFiles/divsufsort.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/public/home/zhoud/vs/c++/kmc/kmer/KMC-master/kmc_dump/FM-index/FM-Index-master/libdivsufsort/lib/divsufsort.c" "lib/CMakeFiles/divsufsort.dir/divsufsort.o" "gcc" "lib/CMakeFiles/divsufsort.dir/divsufsort.o.d"
+  "/public/home/zhoud/vs/c++/kmc/kmer/KMC-master/kmc_dump/FM-index/FM-Index-master/libdivsufsort/lib/sssort.c" "lib/CMakeFiles/divsufsort.dir/sssort.o" "gcc" "lib/CMakeFiles/divsufsort.dir/sssort.o.d"
+  "/public/home/zhoud/vs/c++/kmc/kmer/KMC-master/kmc_dump/FM-index/FM-Index-master/libdivsufsort/lib/trsort.c" "lib/CMakeFiles/divsufsort.dir/trsort.o" "gcc" "lib/CMakeFiles/divsufsort.dir/trsort.o.d"
+  "/public/home/zhoud/vs/c++/kmc/kmer/KMC-master/kmc_dump/FM-index/FM-Index-master/libdivsufsort/lib/utils.c" "lib/CMakeFiles/divsufsort.dir/utils.o" "gcc" "lib/CMakeFiles/divsufsort.dir/utils.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
