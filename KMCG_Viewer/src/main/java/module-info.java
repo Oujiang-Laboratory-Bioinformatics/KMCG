bfd9cd5a642771adac2153f477cc0d7c6364eae8
Carrot Head Oatmeal <49857844+shijianuf@users.noreply.github.com>
2025-11-12 10:28:40 +0800

    Delete KMCG_Viewer/src/main directory

--- KMCG_Viewer/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.apply.kmcg {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.swing;
-
-    opens com.apply.kmcg to javafx.fxml;
-    exports com.apply.kmcg;
-}
